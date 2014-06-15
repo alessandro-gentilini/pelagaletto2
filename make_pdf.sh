72d1d82f9c8cb16ad57e0bc4863aafa569681348
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2014-06-15 14:42:42 +0200

    added some references

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+rm bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.aux
+rm bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.bbl
+rm bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.blg
+rm bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.out
+rm bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.log
+
 pdflatex bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.tex
 bibtex bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.aux
 pdflatex bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.tex
