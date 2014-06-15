7e6b20cc0b9d4e9db5809c8e45028609e7e45a28
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2014-06-15 13:46:47 +0200

    added bibliography

---
+++ make_pdf.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+pdflatex bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.tex
+bibtex bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.aux
+pdflatex bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.tex
+pdflatex bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.tex
+
+rm bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.aux
+rm bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.bbl
+rm bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.blg
+rm bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.out
+rm bibliography_for_non-terminating_game_of_Beggar-My-Neighbour.log
